3025798ff40bebac64c9bc19a2d44ac973bd3fb6
murtalaliyu <murtalaliyu10@gmail.com>
2023-03-28 00:07:29 -0400

    chp3 is WIP

---
+++ C03-ExtendingClasses/code/chp3/ColorAttr.java
@@ -0,0 +1,48 @@
+class ColorAttr extends Attr {
+
+    private ScreenColor myColor;    // the decoded color
+
+    public ColorAttr(String name, Object value) {
+        super(name, value);
+        decodeColor();
+    }
+
+    public ColorAttr(String name) { this(name, "transparent"); }
+
+    public ColorAttr(String name, ScreenColor value) {
+        super(name, value.toString());
+        myColor = value;
+    }
+
+    public Object setValue(Object newValue) {
+        // do the superclass's setValue work first
+        Object retval = super.setValue(newValue);
+        decodeColor();
+        return retval;
+    }
+
+    /** Set value to ScreenColor, not description */
+    public ScreenColor setValue(ScreenColor newValue) {
+        // do the superclass's setValue work first
+        super.setValue(newValue.toString());
+        ScreenColor oldValue = myColor;
+        myColor = newValue;
+        return oldValue;
+    }
+
+    /** Return decoded ScreenColor object */
+    public ScreenColor getColor() {
+        return myColor;
+    }
+
+    /** set ScreenColor from description in getValue */
+    protected void decodeColor() {
+        if (getValue() == null) myColor = null;
+        else myColor = new ScreenColor(getValue());
+    }
+
+}
+
+class ScreenColor {
+    public ScreenColor(Object value) {}
+}
---
+++ C03-ExtendingClasses/code/chp3/PassengerVehicle.java
@@ -0,0 +1,35 @@
+import exercises._2_18.VehicleV4;
+
+/**
+ * Exercise 3.1
+ */
+public class PassengerVehicle extends VehicleV4 {
+
+    private int numberOfSeats;
+    private int numberCurrentlyOccupied;
+
+    public int getNumberOfSeats() { return numberOfSeats; }
+    public void setNumberOfSeats(int numberOfSeats) { this.numberOfSeats = numberOfSeats; }
+    public int getNumberCurrentlyOccupied() { return numberCurrentlyOccupied; }
+    public void setNumberCurrentlyOccupied(int numberCurrentlyOccupied) { this.numberCurrentlyOccupied = numberCurrentlyOccupied; }
+
+    public String toString() {
+        return "Owner Name: " + getOwnerName()
+                + "\n" + "Current Speed: " + getCurrentSpeed()
+                + "\n" + "Current Direction: " + getCurrentDirection()
+                + "\n" + "Number of Seats: " + getNumberOfSeats()
+                + "\n" + "Number Currently Occupied: " + getNumberCurrentlyOccupied();
+    }
+
+    public static void main(String[] args) {
+        PassengerVehicle vehicle = new PassengerVehicle();
+        vehicle.setOwnerName("Mike");
+        vehicle.setCurrentDirection(9);
+        vehicle.setCurrentSpeed(56);
+        vehicle.setNumberOfSeats(7);
+        vehicle.setNumberCurrentlyOccupied(4);
+
+        System.out.println(vehicle);
+    }
+
+}
