9d4eb8bb19c979fcb94a24be8a60e888676a3553
murtalaliyu <murtalaliyu10@gmail.com>
2023-05-09 00:07:18 -0400

    wip, that's the name of the game

---
+++ code/chp4/Attributed.java
@@ -0,0 +1,10 @@
+package chp4;
+
+import chp3.Attr;
+
+public interface Attributed {
+    void add(Attr newAttr);
+    Attr find(String attrName);
+    Attr remove(String attrName);
+    java.util.Iterator<Attr> attrs();
+}
---
+++ code/chp4/AttributedBody.java
@@ -0,0 +1,42 @@
+package chp4;
+
+import chp3.Attr;
+import old.examples.initializationBlocks.Body;
+
+import java.util.Iterator;
+
+class AttributedBody extends Body implements Attributed {
+
+    private AttributedImpl attrImpl = new AttributedImpl();
+
+    public AttributedBody() {
+        super();
+    }
+
+    public AttributedBody(String name, Body orbits) {
+        super(name, orbits);
+    }
+
+    // Forward all Attributed methods to the attrImpl objects
+
+    @Override
+    public void add(Attr newAttr) {
+        attrImpl.add(newAttr);
+    }
+
+    @Override
+    public Attr find(String attrName) {
+        return attrImpl.find(attrName);
+    }
+
+    @Override
+    public Attr remove(String attrName) {
+        return attrImpl.remove(attrName);
+    }
+
+    @Override
+    public Iterator<Attr> attrs() {
+        return attrImpl.attrs();
+    }
+
+}
